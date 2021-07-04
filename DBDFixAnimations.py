ee0f499331e48e29d4ded89b0864873dd7ad57e5
Anime Nyan <tellm3pls@gmail.com>
2021-07-04 14:56:02 +1000

    add buttons to fix broken survivor actions + make refactor code into functions for easy reuse across survivor and killer operator classes

--- DBDFixAnimations.py
+++ DBDFixAnimations.py
@@ -22,81 +22,115 @@ class PSKPSA_PT_fix_dbd_animations_import_panel_2(bpy.types.Panel):
         layout.prop(fixdbdanimtool, "is_problem_jaw_bone")
         
         layout.label(text ="Select the Dead By Daylight Skeleton with problem")
-        layout.label(text ="Animations > Press Fix Dead By Daylight Killer Animation")
-        layout.operator("pskpsa.use_fix_dbd_active_animation_operator")
-        layout.label(text ="To fix ALL Actions > Press Fix ALL Dead By Daylight Killer Actions")
-        layout.operator("pskpsa.fix_all_dbd_actions_operator")
+        layout.label(text ="Animations > Press Fix ONE Dead By Daylight Killer/Survivor Action")
+        layout.operator("pskpsa.fix_dbd_killer_active_action_operator")
+        layout.operator("pskpsa.fix_dbd_survivor_active_action_operator")
+        layout.label(text ="To fix ALL Actions > Press Fix ALL Dead By Daylight Killer/Survivor Actions")
+        layout.operator("pskpsa.fix_all_dbd_killer_actions_operator")
+        layout.operator("pskpsa.fix_all_dbd_survivor_actions_operator")
 
-
-class PSKPSA_OT_fix_dbd_active_animation(bpy.types.Operator):
-    bl_label = "Fix Dead By Daylight Killer Animation"
-    bl_description = "Reset transforms for problem bones for one active Action"
-    bl_idname = "pskpsa.use_fix_dbd_active_animation_operator"
+#---------------fix the active action (only one) for killer and survivor 
+class PSKPSA_OT_fix_dbd_killer_active_action(bpy.types.Operator):
+    bl_label = "Fix ONE Dead By Daylight Killer Action"
+    bl_description = "Reset transforms for problem bones for one Killer active Action"
+    bl_idname = "pskpsa.fix_dbd_killer_active_action_operator"
 
     def execute(self, context):
-        #store active scene to variable
-        scene = context.scene
-        #allow access to user inputted properties through pointer
-        #to properties
-        fixdbdanimtool = scene.fixdbdanim_tool
-
-        is_problem_jaw_bone = fixdbdanimtool.is_problem_jaw_bone
-
-        active_object = bpy.context.active_object
-        if (active_object.type == "ARMATURE"):
-            remove_problem_bones_animations(active_object, is_problem_jaw_bone)
-            success_message = "The Problem Animation has been fixed successfully!"
-            bpy.ops.pskpsa.show_message_operator(message = success_message)
-            log(success_message)
-
-        #throw error message if active object is not a skeleton
-        else:
-            error_message = "Error: Active Object is not an Armature, ensure the active object is the Skeleton with problem animations."
-            bpy.ops.pskpsa.show_message_operator(message = error_message)
-            log(error_message)
+        #True is for killer
+        fix_dbd_killer_or_survivor_active_action(True)
         
         return {'FINISHED'}
 
+class PSKPSA_OT_fix_dbd_survivor_active_action(bpy.types.Operator):
+    bl_label = "Fix ONE Dead By Daylight Survivor Action"
+    bl_description = "Reset transforms for problem bones for one Survivor active Action"
+    bl_idname = "pskpsa.fix_dbd_survivor_active_action_operator"
 
-class PSKPSA_OT_fix_all_dbd_actions(bpy.types.Operator):
+    def execute(self, context):
+        #False is for survivor
+        fix_dbd_killer_or_survivor_active_action(False)
+        
+        return {'FINISHED'}
+
+def fix_dbd_killer_or_survivor_active_action(isKiller):
+    is_problem_jaw_bone = get_is_problem_jaw_bone_var()
+
+    active_object = bpy.context.active_object
+    if (active_object.type == "ARMATURE"):
+        remove_problem_bones_transform_keyframes(active_object, is_problem_jaw_bone, isKiller)
+        success_message = "The Problem Animation has been fixed successfully!"
+        bpy.ops.pskpsa.show_message_operator(message = success_message)
+        log(success_message)
+
+    #throw error message if active object is not a skeleton
+    else:
+        error_message = "Error: Active Object is not an Armature, ensure the active object is the Skeleton with problem animations."
+        bpy.ops.pskpsa.show_message_operator(message = error_message)
+        log(error_message)
+
+
+
+
+
+#---------------fix all the actions action for killer and survivor 
+class PSKPSA_OT_fix_all_dbd_killer_actions(bpy.types.Operator):
     bl_label = "Fix ALL Dead By Daylight Killer Actions"
-    bl_description = "Reset transforms for problem bones for all actions on the skeleton"
-    bl_idname = "pskpsa.fix_all_dbd_actions_operator"
+    bl_description = "Reset transforms for problem bones for all actions on the Killer skeleton"
+    bl_idname = "pskpsa.fix_all_dbd_killer_actions_operator"
 
     def execute(self, context):
-        #store active scene to variable
-        scene = context.scene
-        #allow access to user inputted properties through pointer
-        #to properties
-        fixdbdanimtool = scene.fixdbdanim_tool
+        #True is for Killer
+        fix_all_dbd_killer_or_survivor_actions(True)       
 
-        is_problem_jaw_bone = fixdbdanimtool.is_problem_jaw_bone
+        return {'FINISHED'}
 
-        active_object = bpy.context.active_object
-        if (active_object.type == "ARMATURE"):
-            #iterate through all actions on the skeleton
-            #and remove the animations on problem bones for every action
-            for current_action in bpy.data.actions:
-                #switch active action to the next action
-                active_object.animation_data.action = current_action
-                remove_problem_bones_animations(active_object, is_problem_jaw_bone)
+class PSKPSA_OT_fix_all_dbd_survivor_actions(bpy.types.Operator):
+    bl_label = "Fix ALL Dead By Daylight Survivor Actions"
+    bl_description = "Reset transforms for problem bones for all actions on the Surivivor skeleton"
+    bl_idname = "pskpsa.fix_all_dbd_survivor_actions_operator"
 
-            success_message = "All Problem Animations have been fixed successfully!"
-            bpy.ops.pskpsa.show_message_operator(message = success_message)
-            log(success_message)
+    def execute(self, context):
+        #False is for Survivor
+        fix_all_dbd_killer_or_survivor_actions(False)       
 
-        #throw error message if active object is not a skeleton
-        else:
-            error_message = "Error: Active Object is not an Armature, ensure the active object is the Skeleton with problem animations."
-            bpy.ops.pskpsa.show_message_operator(message = error_message)
-            log(error_message)
-        
         return {'FINISHED'}
 
 
-#this function will remove animations from problem bones and
-#reset their transforms to default
-def remove_problem_bones_animations(active_object, is_problem_jaw_bone):
+def fix_all_dbd_killer_or_survivor_actions(isKiller):
+    is_problem_jaw_bone = get_is_problem_jaw_bone_var()
+    active_object = bpy.context.active_object
+    if (active_object.type == "ARMATURE"):
+        #iterate through all actions on the skeleton
+        #and remove the animations on problem bones for every action
+        for current_action in bpy.data.actions:
+            #switch active action to the next action
+            active_object.animation_data.action = current_action
+            remove_problem_bones_transform_keyframes(active_object, is_problem_jaw_bone, isKiller)
+
+        success_message = "All Problem Animations have been fixed successfully!"
+        bpy.ops.pskpsa.show_message_operator(message = success_message)
+        log(success_message)
+
+    #throw error message if active object is not a skeleton
+    else:
+        error_message = "Error: Active Object is not an Armature, ensure the active object is the Skeleton with problem animations."
+        bpy.ops.pskpsa.show_message_operator(message = error_message)
+        log(error_message)
+
+#----------------common shared functions between fix one active action and all actions 
+
+def get_is_problem_jaw_bone_var():
+    #store active scene to variable
+    scene = bpy.context.scene
+    #allow access to user inputted properties through pointer
+    #to properties
+    fixdbdanimtool = scene.fixdbdanim_tool
+
+    return fixdbdanimtool.is_problem_jaw_bone
+
+#this function will remove keyframes from problem bones and
+#reset their transforms to default for one action
+def remove_problem_bones_transform_keyframes(active_object, is_problem_jaw_bone, isKiller):
     armature = active_object
 
     bpy.ops.object.mode_set(mode='POSE')
@@ -110,20 +144,36 @@ def remove_problem_bones_animations(active_object, is_problem_jaw_bone):
     #the reason why is so that any keyframes
     #used to correct the broken animations are on frame zero
     bpy.context.scene.frame_set(0)
-
+    
+    #this iterates through all the bones
+    #removing keyframes and transforms from every bone
     for bone in armature.pose.bones:
         #use lowercase to catch bones 
         #which have mixed case names
         lowercase_bone_name = bone.name.lower()
 
-        #the problem bones are bones which have roll and ik in their names
-        if("roll" in lowercase_bone_name or "ik" in lowercase_bone_name):
-            remove_animations_from_bone(armature, bone)
-        
-        #the jaw bone sometimes has problems but only on some dbd killer
+        #if this is for killer animations 
+        #remove animations from ik and roll bones
+        if (isKiller):
+            #the problem bones for broken killer animations are bones 
+            #which have roll and ik in their names
+            killer_problem_bones_array = ["roll", "ik"]
+            if(killer_problem_bones_array in lowercase_bone_name):
+                remove_animations_from_bone(armature, bone)
+        else:
+            #the problem bones for broken survivor animations
+            #are bones which have lip, nose, eyelid 
+            #or have eyelt or eyelt in the name
+            survivor_problem_bones_array = ["lip", "nose", "eyelid", "eyert", "eyelt"]
+
+            if(survivor_problem_bones_array in lowercase_bone_name):
+                remove_animations_from_bone(armature, bone)
+
+        #the jaw bone sometimes has problems on some dbd killer and survivor
         #skeletons and not others so we give the user the option to clear
         #transforms or not
 
+        #Check this for both killer and survivor animations
         #if the user has selected to clear transforms
         #on the jaw bone because the animations are not
         #working for it and the current bone name is joint_jaw01
@@ -136,9 +186,10 @@ def remove_problem_bones_animations(active_object, is_problem_jaw_bone):
     bpy.ops.object.mode_set(mode='OBJECT')
 
 
-
+#this function will remove all keyframes
+#and clear transforms for one bone
 def remove_animations_from_bone(armature, bone):
-    #deselect all the bones between each loop of clear
+    #deselect all the bones between each loop of clearing keyframes
     bpy.ops.pose.select_all(action='DESELECT')
 
     #select the bone in pose mode so the transforms can be cleared
@@ -161,7 +212,7 @@ def remove_animations_from_bone(armature, bone):
 
 
 
-#--------------------------------Show feedback to user functions
+#--------------------------------Show feedback to user class and function
 
 #class that is shown when a message needs to 
 #pop up for the user as feedback
@@ -198,8 +249,11 @@ def log(msg):
     print("[PSK/PSA DBD Fix]:", msg)
 
 
-classes = [fix_dbd_animations_properties, PSKPSA_PT_fix_dbd_animations_import_panel_2, 
-PSKPSA_OT_fix_dbd_active_animation, PSKPSA_OT_fix_all_dbd_actions,
+classes = [fix_dbd_animations_properties, PSKPSA_PT_fix_dbd_animations_import_panel_2,
+
+PSKPSA_OT_fix_dbd_killer_active_action, PSKPSA_OT_fix_dbd_survivor_active_action,
+PSKPSA_OT_fix_all_dbd_killer_actions, PSKPSA_OT_fix_all_dbd_survivor_actions,
+
 PSKPSA_OT_show_message]
 
 def register():
